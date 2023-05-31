27df2e543d5aef21b1ccb5c8d3455e5b0321cc31
u418572@kimo.com <u418572@kimo.com>
2023-06-01 03:31:53 +0800

    Initialize Project

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import Github from "components/github"
+import styles from 'components/GitStyle.module.scss'
+function Main () {
+return (
+  <div className={styles.container}>
+<Github />
+  </div>
+)
+
+}
+
+export default Main
---
+++ src/components/github.jsx
@@ -0,0 +1,9 @@
+function Github () {
+  return (
+    <>
+    <h1>這裏測試分支</h1>
+    </>
+  )
+}
+
+export default Github
